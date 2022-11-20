c51b089739896afa8d708e4c9617edfe1f0dc108
yolo <77111763+yoloeww@users.noreply.github.com>
2022-11-20 21:38:46 +0800

    Create 删除公共字符.cpp

---
+++ 删除公共字符.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+     string str1,str2;
+     getline(cin, str1);
+     getline(cin, str2);
+     int hashtable[256] = {0};
+     for(size_t i = 0; i < str2.size(); ++i)
+     {
+        hashtable[str2[i]]++;
+     } 
+     string ret;
+     for(size_t i = 0; i < str1.size(); ++i)
+     {
+         if(hashtable[str1[i]] == 0)
+         ret += str1[i];
+     }
+     cout<<ret<<endl;
+     return 0;
+}
